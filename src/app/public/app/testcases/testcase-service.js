49469671578187b1eb87975138ec09fa4feaaaee
Jussi Vatjus-Anttila <jussiva@gmail.com>
2014-10-13 09:29:52 +0300

    moved frontend code to separate git-repo. gui is like one of the addons .

--- src/app/public/app/testcases/testcase-service.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('tmtServices')
-  .factory('Testcase', function($resource) {
-    return $resource(
-	    "/api/v0/testcases/:id", {id: '@_id'},
-	    {update: {method: "PUT", isArray: false}}
-    );
-  })
