bdc42a7afd4e7e61ed12ee019fbe8ad7c0683b58
Jussi Vatjus-Anttila <jussiva@gmail.com>
2015-08-16 14:25:43 +0300

    Update Result DB schema

--- src/app/models/results.js
+++ src/app/models/results.js
@@ -7,28 +7,46 @@ var mongoose = require('mongoose');
 //var userPlugin = require('mongoose-user');
 var Schema = mongoose.Schema;
 var QueryPlugin = require('mongoose-query');
+
 /**
  * User schema
  */
-
 var ResultSchema = new Schema({
   tcid: { type: String, required: true },
-  tcRef: {type: String },
+  tcRef: {type: Schema.Types.ObjectId, ref: 'Testcase' },
   cre: {
     time: {type: Date, default: Date.now},
-    user: {type: String}
-  },
-  verdict: {
-    final: {type: String, required: true, enum: ['pass', 'fail', 'inconclusive', 'blocked', 'error'] }
-  },
+    user: {type: String},
+    userRef: {type: Schema.Types.ObjectId, ref: 'User' } 
+  },  
   exec: {
+    verdict: { type: String, required: true, enum: ['pass', 'fail', 'inconclusive', 'blocked', 'error'] },
     duration: {type: Number}, //seconds
     framework: {
       name: {type: String, enum: ['clitest'], required: true},
       ver: {type: String, required: true},
     },
+    sw: {
+      ref: {type: Schema.Types.ObjectId, ref: 'Build' },
+      branch: {type: String},
+      commitId: {type: String},
+      href: {type: String}
+    },
+    env: {
+      ref: {type: Schema.Types.ObjectId, ref: 'Resource' },
+      rackId: {type: String},
+    },
+    dut: {
+      ref: {type: Schema.Types.ObjectId, ref: 'Resource' },
+      vendor: {type: String},
+      model: {type: String},
+      ver: {type: String},
+      sn: {type: String}
+    },
     logs: [
       {
+        ref: {type: Schema.Types.ObjectId, ref: 'Resource' },
+        from: {type: String, enum: ['dut', 'framework', 'env', 'other']},
         filename: {type: String},
         filesize: {type: Number},
         refs: {type: String},
@@ -36,7 +54,6 @@ var ResultSchema = new Schema({
       }
     ]
   }
-  //...
 });
 
 /**
