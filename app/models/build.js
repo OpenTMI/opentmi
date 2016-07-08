bfdc358a05ee6583aa1bb84ffab332673ea83da9
Jussi Vatjus-Anttila <jupe@users.noreply.github.com>
2016-07-08 07:19:09 +0300

    Update build schema (#7)

--- app/models/build.js
+++ app/models/build.js
@@ -2,28 +2,49 @@
 /*!
  * Module dependencies
  */
+// native modules
+var crypto = require('crypto');
 
+// 3rd party modules
 var mongoose = require('mongoose');
 var QueryPlugin = require('mongoose-query');
 
 
+function checksum (str, algorithm, encoding) {
+    return crypto
+        .createHash(algorithm || 'sha1')
+        .update(str, 'binary')
+        .digest(encoding || 'hex')
+}
+
 var Schema = mongoose.Schema;
+
 /**
  * Build schema
  */
 var BuildSchema = new Schema({
-  
   name: {type: String, required: true },
-  uuid: {type: String },
-  v_ctrl: {type: String, enum: ['git','SVN', 'CSV']},
-  repository: [
-    new Schema({ 
-      url: { type: String }
+  type: { type: String, enum: ['cliapp', 'unittest']},
+  cre: {
+    user: {type: String},
+    time: {type: Date, default: Date.now},
+  },
+  mod: {
+    user: {type: String},
+    time: {type: Date, default: Date.now},
+  },
+  uuid: { type: String },
+  vcs: [
+    new Schema({
+      name: { type: String },
+      type: { type: String, enum: ['git','SVN', 'CSV'] }, //version control system
+      url: { type: String },
+      commitId: { type: String },
+      clean_wa: { type: Boolean }, 
+      branch: { type: String },
     })
   ],
-  commitId: {type: String }, //sha-1
-  branch: { type: String },
-  compiledBy: {type: String, enum: ['CI', 'manual']},
+  compiledBy: { type: String, enum: ['CI', 'manual'] },
   changeId: {type: String}, // e.g. when gerrit build
   pullRequest: {type: String}, // e.g. when github
   meta_info: {
@@ -35,7 +56,9 @@ var BuildSchema = new Schema({
     //buffer limit 16MB when attached to document!
     name: { type: String },
     data: { type: Buffer },
-    size: { type: Number }
+    size: { type: Number },
+    sha1: { type: String },
+    sha256: { type: String }
   },
   location: [
     new Schema({
@@ -46,13 +69,20 @@ var BuildSchema = new Schema({
       },
     })
   ],
+  issues: [
+    new Schema({
+      time: {type: Date, default: Date.now},
+      type: { type: String, enum: ['github', 'jira'] },
+      url: { type: String }
+    })
+  ],
   // build target device
   target: {
-    type: { type: String, enum: ['simulate','hardware'], required: true},
+    type: { type: String, enum: ['simulate','hardware'], default: 'hardware'},
     os: { type: String, enum: ['win32', 'win64', 'linux32', 'linux64', 'mbedOS', 'unknown'] },
     simulator: {
-      bt: {type: String},
-      network: {type: String},
+      bt: { type: String },
+      network: { type: String },
     },
     hw: {
       vendor: { type: String },
@@ -82,11 +112,18 @@ BuildSchema.path('location').validate(function (value, respond) {
     if( value.length === 0 ) respond(false);
     else  respond(true);
  }, '{PATH} missing');
+ */
 
 BuildSchema.pre('validate', true, function (next, done) {
 
-  if( this.target.type === 'simulate' ){ 
-      next();
+  if( this.build && this.build.data ) {
+    this.build.size = this.build.data.length;
+    this.build.sha1 = checksum(this.build.data, 'sha1');
+    this.build.sha256 = checksum(this.build.data, 'sha256');
+  }
+  if( this.target.type === 'simulate' ){
+    if( this.target.simulator ) next();
+    else next('simulator missing');
   } else if( this.target.type === 'hardware' ){ 
     if( this.target.hw ) next();
     else next( 'target missing' );
@@ -94,23 +131,16 @@ BuildSchema.pre('validate', true, function (next, done) {
     next( 'target missing' );
   }
  }, '{PATH}: invalid');
-*/
 
 /**
  * Methods
  */
-
-BuildSchema.method({
-
-});
+//BuildSchema.method({});
 
 /**
  * Statics
  */
-
-BuildSchema.static({
-
-});
+//BuildSchema.static({});
 
 /**
  * Register
