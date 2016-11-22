e9b2bed2504ac6ee019d3388d40cd31b20a38e65
Eero Paavola <paavola.ee@gmail.com>
2016-11-22 14:03:53 +0200

    Modify test data, add start script for windows ('startWin')

--- test/loans.js
+++ test/loans.js
@@ -2,12 +2,13 @@
 var superagent = require("superagent"),
     chai = require("chai"),
     expect = chai.expect,
+    mongoose = require("mongoose"),
     should = require("should");
 
 var api = "http://localhost:3000/api/v0"
 
 describe("Loans", function () {
- 
+  var first_loan_id;
   it("should return ALL loans on /loans GET", function (done) {
     superagent.get(api+"/loans")
       .type('json')
@@ -17,14 +18,14 @@ describe("Loans", function () {
         expect(e).to.equal(null);
         expect(res.body).not.to.be.empty;
         expect(res.body).to.be.an('array');
-        single_loan = res.body[0];
+        first_loan_id = res.body[0]["_id"].toString();
         done();
       })
 
     });
-/*
+
   it("should return a SINGLE loan on /loans/<id> GET", function (done) {
-    superagent.get(api + "/loans" + "?_id=" + loan_id)
+    superagent.get(api + "/loans" + "?_id=" + first_loan_id)
       .type('json')
       .end(function (e, res) {
         res.should.be.json
@@ -38,20 +39,55 @@ describe("Loans", function () {
 
         var item_data = res.body[0];
         expect(item_data).to.have.property('_id');
-        expect(item_data).to.have.property('count');
-        expect(item_data.count).to.be.at.least(1);
-        expect(item_data).to.have.property('date_loaned');
         expect(item_data).to.have.property('loaner');
-        expect(item_data).to.have.property('loaned_item');
-        
+        expect(item_data).to.have.property('items');
+        expect(item_data.items).to.be.an('array');
+        expect(item_data.items[0]).to.have.property('item');
+        expect(item_data).to.have.property('loan_date');
         done();
       });
   });
-*/
-    
-  it("should add a SINGLE loan on /loans POST");
-    
-  it("should update a SINGLE item on /loans/<id> PUT");
+
+  var new_loan_id;
+  it("should add a SINGLE loan on /loans POST", function (done) {
+	  var date = new Date();
+    var body = {
+      "loan_date" : date,
+      "loaner" : "5825bb7cfe7545132c88c777",                  
+      "items" : 
+        [
+          {"item" : "582c7948850f298a5acff983"}
+        ]
+    };
+
+    superagent.post(api+'/loans')
+      .send(body)
+      .end(function (e, res) {
+        res.should.be.json
+        if( res.status === 300 ) {
+          console.log("seems that your DB is not clean!");
+          process.exit(1);
+        }
+        expect(res.status).to.equal(200);
+        expect(e).to.equal(null);
+        done();
+      });
+  }); 
+  
+  // FIXME
+  it.skip("should update a SINGLE item on /loans/<id> PUT", function (done) {    
+    superagent.put(api + '/loans/' + new_item_id)
+      .send(body)
+      .end(function (e, res) {
+        res.should.be.json
+        if( res.status === 300 ) {
+          console.log("seems that your DB is not clean!");
+          process.exit(1);
+        }
+        expect(res.status).to.equal(200);
+        expect(e).to.equal(null);
+      });
+  });
 
   // In normal use loans are not deleted (in order to maintain loan history)
   // Add this when core features work
