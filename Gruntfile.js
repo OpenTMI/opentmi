6d533550104ff912a1980d19ba693fafedc3ac3d
Olli Korhonen <ollkor01@arm.com>
2017-06-26 16:02:42 +0300

    included db logs to collected artefacts and temporarily unsuppressed dbrestore logging

--- Gruntfile.js
+++ Gruntfile.js
@@ -33,8 +33,8 @@ module.exports = function (grunt) {
       },
       restore_db_linux: {
         cmd: './scripts/dbrestore_linux.sh local ./test/seeds/test_dump/',
-        stdout: false,
-        stderr: false,
+        stdout: true,
+        stderr: true,
         options: {
           shell: 'bash',
         },
