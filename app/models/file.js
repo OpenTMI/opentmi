b8be7c85c241178c60d4ace30d4dea10870372ee
Olli Korhonen <olkorhon@gmail.com>
2017-07-07 14:44:07 +0300

    Filedb rewrite (#43)
    * first midpoint of filedb rewrite. Classes defined and commented, logging still needs a proper check at least
    * refactored redundant File class away, filedb now again coupled with FileSchema. Also refactored some file responsibilities away from build model to file model
    * Filedb rewrite tests (#45)
    * Auth logging update (#42)
    * increased logging during authentication and also slightly refactored authentication
    * changed multiple enveloppers into a single addPrefix for simplicity + refactored constructor away since it serves no purpose.
    * redirected nconf calls to more controllable singleton package. (#44)
    * added unittests for new filedb + fixed filedb to pass these new tests
    * base64 changed to encoding + cleanups
    * made requested changes + rename of _resolveFilename to more fitting _resolveFilePath

--- app/models/file.js
+++ app/models/file.js
@@ -1,32 +1,84 @@
-//native modules
-var path = require('path');
+// native modules
+const path = require('path');
 
 // 3rd party modules
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
+const logger = require('winston');
+const mongoose = require('mongoose');
 
-var nconf = require('../../config');
-var file_provider = nconf.get('filedb');
+// local module
+const nconf = require('../../config');
+const checksum = require('../tools/checksum.js');
+const fileProvider = nconf.get('filedb');
 
-var FileSchema = new Schema({
-    //buffer limit 16MB when attached to document!
-    name: {type: String},
-    mime_type: {type: String},
-    base64: {type: String},
-    data: {type: Buffer},
-    size: {type: Number},
-    sha1: {type: String},
-    sha256: {type: String}
+const FileSchema = new mongoose.Schema({
+  // buffer limit 16MB when attached to document!
+  name: { type: String },
+  mime_type: { type: String },
+  encoding: { type: String, enum: ['raw', 'base64'], default: 'raw' },
+  data: { type: Buffer },
+  size: { type: Number },
+  sha1: { type: String },
+  sha256: { type: String }
 });
-FileSchema.set('toObject', {virtuals: true});
+FileSchema.set('toObject', { virtuals: true });
 
-FileSchema.virtual('hrefs').get(
-    function () {
-        var href;
-        if (file_provider && file_provider !== 'mongodb' && this.sha1) {
-            href = path.join(file_provider, this.sha1);
-        }
-        return href;
-    });
+FileSchema.virtual('hrefs').get(function () {
+  const hasHref = fileProvider && (fileProvider !== 'mongodb') && this.sha1;
+  const pathToFile = path.join(fileProvider, this.sha1);
+  return hasHref ? pathToFile : undefined;
+});
 
+FileSchema.methods.prepareDataForStorage = function () {
+  logger.info(`Preparing file (name: ${this.name}) for storage.`);
+
+  if (this.encoding === 'base64') {
+    logger.debug('Base64 file detected, storing data to a buffer.');
+    this.data = new Buffer(this.data, 'base64');
+  }
+
+  if (this.data) {
+    this.size = this.data.length;
+    // file.type = mimetype(file.name(
+    this.sha1 = checksum(this.data, 'sha1');
+    this.sha256 = checksum(this.data, 'sha256');
+  }
+};
+
+FileSchema.methods.storeInFileDB = function () {
+  // filedb is reuired here because it causes a circular dependency otherwise
+  const filedb = require('../tools/filedb.js'); // eslint-disable-line
+  return filedb.storeFile(this).catch((error) => {
+    logger.error(`Could not save file to filedb, reason: ${error.message}.`);
+    throw error;
+  });
+};
+
+FileSchema.methods.retrieveFromFileDB = function () {
+  // filedb is reuired here because it causes a circular dependency otherwise
+  const filedb = require('../tools/filedb.js'); // eslint-disable-line
+  return filedb.readFile(this).then((data) => {
+    this.data = data;
+    return data;
+  }).catch((error) => {
+    logger.error(`Could not read file from filedb, reason: ${error.message}.`);
+    throw error;
+  });
+};
+
+FileSchema.methods.checksum = function () {
+  if (!this.sha1) {
+    logger.warn('File without sha1 checksum processed, prepareDataForStorage not called?');
+
+    if (this.data) {
+      this.sha1 = checksum(this.data, 'sha1');
+    } else {
+      logger.warn('Could not calculate checksum for file without data.');
+      return null;
+    }
+  }
+
+  return this.sha1;
+};
+
+mongoose.model('File', FileSchema);
 module.exports = FileSchema;
