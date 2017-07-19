57e5c1d88550c59ba87c9fa592c4b6e6a57fe7c6
Olli Korhonen <ollkor01@arm.com>
2017-07-19 09:40:16 +0300

    lintified unittests mocking

--- test/tests_unittests/mocking/MockItems.js
+++ test/tests_unittests/mocking/MockItems.js
@@ -7,40 +7,40 @@ const mockItems = [
     name: 'das_item',
     in_stock: 35,
     available: 10,
-    category: 'board',
+    category: 'board'
   },
   {
     _id: mongoose.Types.ObjectId(),
     name: 'void_item',
     in_stock: 12,
     available: 8,
-    category: 'board',
+    category: 'board'
   },
   {
     _id: mongoose.Types.ObjectId(),
     name: 'testing_item',
     in_stock: 9,
     available: 8,
-    category: 'accessory',
+    category: 'accessory'
   }, {
     _id: mongoose.Types.ObjectId(),
     name: 'another_item',
     in_stock: 15,
     available: 6,
-    category: 'board',
+    category: 'board'
   }, {
     _id: mongoose.Types.ObjectId(),
     name: 'third_item',
     in_stock: 3,
     available: 2,
-    category: 'accessory',
+    category: 'accessory'
   }, {
     _id: mongoose.Types.ObjectId(),
     name: 'fourth_item',
     in_stock: 100,
     available: 67,
-    category: 'board',
-  },
+    category: 'board'
+  }
 ];
 
 module.exports = mockItems;
