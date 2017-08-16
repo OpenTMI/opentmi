e0f90bd95dd2c32604657a2057f37fe1beab2f57
Jussi Vatjus-Anttila <jusvat01@e105203.emea.arm.com>
2017-08-16 11:15:47 +0300

    add user settings related routes

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -3,15 +3,52 @@
 */
 
 // 3rd party modules
+const _ = require('lodash');
 
 // Own modules
 const DefaultController = require('./');
+
 
 class UsersController extends DefaultController {
   constructor() {
     super('User');
   }
+  deleteSettings(req, res) {
+    const namespace = req.params.Namespace;
+    this.Model.findById(req.user.sub).then(
+      (user) => {
+        if (_.has(user, `settings.${namespace}`)) {
+          user.settings[namespace] = {}; // eslint-disable-line no-param-reassign
+          user.markModified('settings');
+          user.save();
+          res.send({message: 'settings cleared'});
+        } else {
+          res.send({message: 'no settings to clear'});
+        }
+      }
+    );
+  }
+  getSettings(req, res) {
+    const namespace = req.params.Namespace;
+    this.Model.findById(req.user.sub).then(
+      (user) => {
+        const settings = _.get(user, `settings.${namespace}`) || {};
+        res.send(settings);
+      }
+    );
+  }
+  updateSettings(req, res) {
+    const namespace = req.params.Namespace;
+    this.Model.findById(req.user.sub).then(
+      (user) => {
+        _.defaults(user, {settings: {}});
+        user.settings[namespace] = req.body.settings; // eslint-disable-line no-param-reassign
+        user.markModified('settings');
+        user.save();
+        res.send({message: 'settings saved'});
+      }
+    );
+  }
 }
-
 
 module.exports = UsersController;
--- app/routes/users.js
+++ app/routes/users.js
@@ -46,39 +46,53 @@ function Route(app) {
   });
 
   // Create user routes
-  const router = express.Router();
-  router.param('User', userController.modelParam.bind(userController));
+  const userRouter = express.Route();
+  userRouter.param('User', userController.modelParam.bind(userController));
 
   // Route for operations that target all users
-  router.route('/api/v0/users.:format?')
+  userRouter.route('/.:format?')
     .get(jwt({secret: TOKEN_SECRET}), auth.ensureAdmin, userController.find.bind(userController))
     .post(jwt({secret: TOKEN_SECRET}), auth.ensureAdmin, userController.create.bind(userController));
 
   // Route for operations that target individual users
-  router.route('/api/v0/users/:User.:format?')
+  userRouter.route('/:User.:format?')
     .get(jwt({secret: TOKEN_SECRET}), auth.ensureAdmin, userController.get.bind(userController))
     .put(jwt({secret: TOKEN_SECRET}), auth.ensureAdmin, userController.update.bind(userController))
     .delete(jwt({secret: TOKEN_SECRET}), auth.ensureAdmin, userController.remove.bind(userController));
 
-  app.use(router);
+
+  // Create User settings routes
+  const settingsRoute = express.Route();
+  settingsRoute.route('/:Namespace')
+    .get(jwt({secret: TOKEN_SECRET}), auth.ensureAuthenticated, userController.getSettings.bind(userController))
+    .put(jwt({secret: TOKEN_SECRET}), auth.ensureAuthenticated, userController.updateSettings.bind(userController))
+    .delete(jwt({secret: TOKEN_SECRET}), auth.ensureAuthenticated, userController.deleteSettings.bind(userController));
+  userRouter.use('/settings', settingsRoute);
+
+  // allows to use /client-settings instead of /settings
+  userRouter.use('/client-settings/', express.Route().all((req) => { req.redirect('../settings'); }));
 
   // Create authentication routes:
   app.get('/api/v0/apikeys', jwt({secret: TOKEN_SECRET}), auth.ensureAdmin, apiKeys.keys);
-  app.get('/api/v0/users/:User/apikeys', jwt({secret: TOKEN_SECRET}), auth.ensureAuthenticated, apiKeys.userKeys);
-  app.get('/api/v0/users/:User/apikeys/new', jwt({secret: TOKEN_SECRET}), auth.ensureAuthenticated, apiKeys.createKey);
-  app.delete('/api/v0/users/:User/apikeys/:Key', jwt({secret: TOKEN_SECRET}), auth.ensureAuthenticated,
-    apiKeys.deleteKey);
+  userRouter
+    .get('/apikeys', jwt({secret: TOKEN_SECRET}), auth.ensureAuthenticated, apiKeys.userKeys)
+    .get('/apikeys/new', jwt({secret: TOKEN_SECRET}), auth.ensureAuthenticated, apiKeys.createKey)
+    .delete('/apikeys/:Key', jwt({secret: TOKEN_SECRET}), auth.ensureAuthenticated, apiKeys.deleteKey);
 
-  app.post('/auth/login', authController.login.bind(authController));
-  app.get('/auth/me', jwt({secret: TOKEN_SECRET}), auth.ensureAuthenticated,
-    authController.getme.bind(authController));
-  app.put('/auth/me', jwt({secret: TOKEN_SECRET}), auth.ensureAuthenticated,
-    authController.putme.bind(authController));
-  app.post('/auth/signup', authController.signup.bind(authController));
-  app.post('/auth/logout', authController.logout.bind(authController));
-  app.post('/auth/github', jwt({secret: TOKEN_SECRET, credentialsRequired: false}), AuthController.github);
-  app.post('/auth/google', authController.google.bind(authController));
-  app.get('/auth/github/id', AuthController.getGithubClientId);
+  // register user router
+  app.use('/api/v0/users', userRouter);
+
+  const authRoute = express.Route();
+  authRoute
+    .post('/login', authController.login.bind(authController))
+    .get('/me', jwt({secret: TOKEN_SECRET}), auth.ensureAuthenticated, authController.getme.bind(authController))
+    .put('/me', jwt({secret: TOKEN_SECRET}), auth.ensureAuthenticated, authController.putme.bind(authController))
+    .post('/signup', authController.signup.bind(authController))
+    .post('/logout', authController.logout.bind(authController))
+    .post('/google', authController.google.bind(authController))
+    .post('/github', jwt({secret: TOKEN_SECRET, credentialsRequired: false}), AuthController.github)
+    .get('/github/id', AuthController.getGithubClientId);
+  app.use('/auth', authRoute);
 
   /**
    * Empty callback route for addon OAuth 2 authentication flow to retain passed parameters in the url.
