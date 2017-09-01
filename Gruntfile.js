de15ee9f38d4b95376b5a8d11991b1acc202e9c6
Olli Korhonen <ollkor01@arm.com>
2017-09-01 17:10:06 +0300

    cleaned rest of the gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,108 +1,125 @@
-require('xunit-file');
+// Native components
 const fs = require('fs');
 const path = require('path');
 
+// Third party components
+require('xunit-file');
+
+// Grunt variables
 const dbPath = path.join('.', 'scripts', 'dbrestore.sh');
 const dumpPath = path.join('.', 'test', 'seeds', 'test_dump');
 
-function gruntSetup(grunt) {
-  const testFilesApi = ['test/tests_api/*.js'];
-  const testFilesUnit = [
-    'test/tests_unittests/*.js',
-    'test/tests_unittests/tools/*.js',
-    'test/tests_unittests/tools/**/*.js',
-    'test/tests_unittests/addons/*.js',
-    'test/tests_unittests/controllers/*.js',
-    'test/tests_unittests/routes/*.js'
-  ];
+const testFilesApi = ['test/tests_api/*.js'];
+const testFilesUnit = [
+  'test/tests_unittests/*.js',
+  'test/tests_unittests/tools/*.js',
+  'test/tests_unittests/tools/**/*.js',
+  'test/tests_unittests/addons/*.js',
+  'test/tests_unittests/controllers/*.js',
+  'test/tests_unittests/routes/*.js'
+];
 
-  grunt.initConfig({
-    express: {
-      test: {
-        options: {
-          delay: 15000,
-          script: 'index.js',
-          node_env: 'test',
-          args: ['-s'] // to more traces set -vvv instead of -s (silent)
-        }
-      }
-    },
-    waitServer: {
-      server: {
-        options: {
-          timeout: 30000,
-          url: 'http://localhost:3000/api/v0'
-        }
-      }
-    },
-    exec: {
-      restore_db: {
-        cmd: `bash ${dbPath} local ${dumpPath}`,
-        stdout: false,
-        stderr: false
-      }
-    },
-    simplemocha: {
+const gruntConfig = {
+  express: {
+    test: {
       options: {
-        globals: ['should', 'check'],
-        timeout: 120000,
-        ignoreLeaks: false
-      },
-      api: {
-        src: testFilesApi
-      },
-      unit: {
-        src: testFilesUnit
+        delay: 15000,
+        script: 'index.js',
+        node_env: 'test',
+        args: ['-s'] // to more traces set -vvv instead of -s (silent)
       }
     }
-  });
-
-  grunt.registerTask('FindApiTests', 'Find all tests under the addons', () => {
-    const root = 'app/addons';
-    function walk(_path) {
-      const items = fs.readdirSync(_path);
-      items.forEach((item) => {
-        const dirName = `${_path}/${item}`;
-        if (fs.statSync(dirName).isDirectory()) {
-          // find only directories that ends with /test
-          if (dirName.substr(dirName.length - 5) === '/test') {
-            // use only test directories that are in the root of the addon
-            if ((dirName.match(/\//g) || []).length === 3) {
-              // only add test js files that are in the root of the /test directory.
-              // if you need to include subdirs, modify this to: dirName + '/' + '**/*.js'
-              testFilesApi.push(`${dirName}/*.js`);
-            }
-          }
-          walk(dirName);
-        }
-      });
+  },
+  waitServer: {
+    server: {
+      options: {
+        timeout: 30000,
+        url: 'http://localhost:3000/api/v0'
+      }
     }
-    walk(root);
+  },
+  exec: {
+    restore_db: {
+      cmd: `bash ${dbPath} local ${dumpPath}`,
+      stdout: false,
+      stderr: false
+    }
+  },
+  simplemocha: {
+    options: {
+      globals: ['should', 'check'],
+      timeout: 120000,
+      ignoreLeaks: false
+    },
+    api: {
+      src: testFilesApi
+    },
+    unit: {
+      src: testFilesUnit
+    }
+  }
+};
+
+function listAddons() {
+  const root = 'app/addons';
+
+  return fs.readdirSync(root)
+    .map(item => path.join(root, item)) // Map items to actual paths to those items
+    .filter(itemPath => fs.statSync(itemPath).isDirectory()); // Filter only directories
+}
+
+function findAddonUnitTests() {
+  listAddons().forEach((addonPath) => {
+    // Read addon items
+    fs.readdirSync(addonPath)
+      .filter(item => item === 'unitTests') // Filter only items that are named unitTests
+      .map(item => path.join(addonPath, item)) // Map items to actual paths to those items
+      .filter(itemPath => fs.statSync(itemPath).isDirectory()) // filter only those paths that are directories
+      .forEach((testPath) => {
+        testFilesUnit.push(path.join(testPath, '*.js'));
+      });
   });
+}
+
+function findAddonApiTests() {
+  listAddons().forEach((addonPath) => {
+    // Read addon items
+    fs.readdirSync(addonPath)
+      .filter(item => item === 'apiTests') // Filter only items that are named apiTests
+      .map(item => path.join(addonPath, item)) // Map items to actual paths to those items
+      .filter(itemPath => fs.statSync(itemPath).isDirectory()) // filter only those paths that are directories
+      .forEach((testPath) => {
+        testFilesApi.push(path.join(testPath, '*.js'));
+      });
+  });
+}
+
+function gruntSetup(grunt) {
+  grunt.initConfig(gruntConfig);
 
   grunt.loadNpmTasks('grunt-wait-server');
   grunt.loadNpmTasks('grunt-express-server');
   grunt.loadNpmTasks('grunt-exec');
   grunt.loadNpmTasks('grunt-simple-mocha');
 
-  grunt.registerTask('default', [
-    'FindApiTests',
-    'simplemocha:unit',
-    'express:test',
-    'waitServer',
-    'exec:restore_db',
-    'simplemocha:api'
-  ]);
+  grunt.registerTask('findAddonUnitTests', findAddonUnitTests);
+  grunt.registerTask('findAddonApiTests', findAddonApiTests);
 
+  grunt.registerTask('unittests', [
+    'findAddonUnitTests',
+    'simplemocha:unit'
+  ]);
   grunt.registerTask('apitests', [
-    'FindApiTests',
+    'findAddonApiTests',
     'exec:restore_db',
     'express:test',
     'waitServer',
     'simplemocha:api'
   ]);
-
-  grunt.registerTask('unittests', ['simplemocha:unit']);
+  grunt.registerTask('default', [
+    'unittests',
+    'apitests'
+  ]);
 }
 
 
