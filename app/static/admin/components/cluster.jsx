027cca26f1d8ec65d680ca9c2d1a276357eff24f
Jussi Vatjus-Anttila <jussiva@gmail.com>
2017-09-28 14:13:06 +0300

    tuning admin ui - workers table

--- app/static/admin/components/cluster.jsx
+++ app/static/admin/components/cluster.jsx
@@ -1,31 +1,104 @@
 class ClusterStatus extends React.Component {
   constructor(props) {
     super(props);
-    this.state  = {status: ''};
-    this._headers = {headers: {Authorization: `Bearer ${this.props.token}`}};
-    setTimeout(this.updateStatus.bind(this),1000);
+    this.state  = {
+      clusters: {workers: []},
+      loading: false, version: {}};
+    this._restOptions = {
+      timeout: 500,
+      headers: {Authorization: `Bearer ${this.props.token}`}};
+    setInterval(this.updateData.bind(this), 1000);
   }
   reload() {
-    /*axios
-    .post('/api/v0/clusters/reset', this._headers)
-      .then((response) => {
-        this.setState({status: response.data});
-      });*/
-  }
-  updateStatus() {
     axios
-    .get('/api/v0/clusters', this._headers)
+    .post('/api/v0/restart', this._restOptions)
       .then((response) => {
-        this.setState({status: response.data});
+        //this.setState({restart: response.data});
       });
   }
+  version() {
+    axios
+    .get('/api/v0/version', this._restOptions)
+      .then((response) => {
+        this.setState({version: response.data});
+      });
+  }
+  updateData() {
+    // show the loading overlay
+    this.setState({loading: true})
+    // fetch your data
+    this.getClusters()
+      .then((data) => {
+        this.setState({clusters: data});
+      })
+      .then(() => {
+        // Update react-table
+        this.setState({loading: false});
+      })
+  }
+  getClusters() {
+    return axios
+    .get('/api/v0/clusters', this._restOptions)
+      .then((response) => {
+        return response.data;
+      })
+      //.catch(() => {workers: []});
+  }
   render() {
+    const ReactTable = window.ReactTable.default;
     return (
       <div>
-      <button onClick="reload" value="reload workers"/>
-      <pre id="status">
-      {JSON.stringify(this.state.status, null, 2)}
-      </pre>
+        <button onClick={this.reload.bind(this)}>reload workers</button>
+        <ReactTable
+          data={this.state.clusters.workers}
+          loading={this.state.loading}
+          onFetchData={(state, instance) => {
+            this.updateData();
+          }}
+          columns={[
+            {
+              Header: "Workers",
+              columns: [
+                {
+                  Header: "ID",
+                  accessor: "id"
+                },
+                {
+                  Header: "PID",
+                  id: "pid",
+                  accessor: o => `${o.pid}`
+                },
+                {
+                  Header: "status",
+                  accessor: 'status',
+                  Cell: row => (
+                    <span>
+                      <span style={{
+                        color: row.row._original.isDead ? '#ff2e00'
+                          : row.row._original.closing ? '#ffbf00'
+                          : '#57d500',
+                        transition: 'all .3s ease'
+                      }}>
+                        &#x25cf;
+                      </span> {
+                        row.row._original.isDead ? 'DEAD'
+                        : row.row._original.starting ? 'starting'
+                        : row.row._original.isConnected ? 'OK'
+                        : row.row._original.closing ? 'closing'
+                        : '?!?!'
+                      }
+                    </span>)
+                }
+              ]
+            }
+          ]}
+          minRows={4}
+          showPageJump={false}
+          showPagination={false}
+          showPaginationBottom={false}
+          showPageSizeOptions={false}
+          className="-striped -highlight"
+        />
       </div>
     );
   }
