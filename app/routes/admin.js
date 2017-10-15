9e25afdcaea8c5064178d595b93f4f65a4e5eee1
Jussi Vatjus-Anttila <jupe@users.noreply.github.com>
2017-10-15 13:03:22 +0300

    create static Promise function for stats reading from master (#117)
    If cluster is not in use return "not found" 404 response when calling some cluster related API

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,3 +1,6 @@
+// native modules
+const cluster = require('cluster');
+
 // Third party modules
 const express = require('express');
 const jwt = require('express-jwt');
@@ -6,6 +9,7 @@ const jwt = require('express-jwt');
 const nconf = require('../../config');
 const auth = require('./../../config/middlewares/authorization');
 const AdminController = require('./../controllers/admin');
+const {notClustered} = require('./../controllers/common');
 
 // Route variables
 const TOKEN_SECRET = nconf.get('webtoken');
@@ -18,8 +22,14 @@ function Route(app) {
     .get(jwt({secret: TOKEN_SECRET}), auth.ensureAdmin, controller.version.bind(controller))
     .post(jwt({secret: TOKEN_SECRET}), auth.ensureAdmin, controller.update.bind(controller));
 
+  let restart;
+  if (!cluster.isMaster && cluster.worker.isConnected()) {
+    restart = controller.restart.bind(controller);
+  } else {
+    restart = notClustered;
+  }
   router.route('/api/v0/restart')
-    .post(jwt({secret: TOKEN_SECRET}), auth.ensureAuthenticated, controller.restart.bind(controller));
+      .post(jwt({secret: TOKEN_SECRET}), auth.ensureAuthenticated, restart);
 
   app.use(router);
 }
