016786eac62c6364aacea2a058daeb58a46e5762
Jussi Vatjus-Anttila <jusvat01@C02TF16WHF1T.local>
2018-01-16 14:57:22 +0200

    tuning tests

--- test/tests_unittests/controllers/builds.js
+++ test/tests_unittests/controllers/builds.js
@@ -1,17 +1,12 @@
 /* eslint-disable func-names, prefer-arrow-callback, no-unused-expressions */
 
 // Third party components
-require('colors');
 const chai = require('chai');
 const chaiSubset = require('chai-subset');
 const mongoose = require('mongoose');
 const Mockgoose = require('mockgoose').Mockgoose;
 const logger = require('winston');
 const Promise = require('bluebird');
-
-// Local components
-require('../../../app/models/build.js');
-const BuildsController = require('../../../app/controllers/builds.js');
 
 // Setup
 logger.level = 'error';
@@ -23,15 +18,23 @@ const mockgoose = new Mockgoose(mongoose);
 const expect = chai.expect;
 
 describe('controllers/builds.js', function () {
+  let BuildsController;
   // Create fresh DB
   before(function () {
     mockgoose.helper.setDbVersion('3.2.1');
 
-    logger.debug('[Before] Preparing storage'.gray);
-    return mockgoose.prepareStorage().then(() => {
-      logger.debug('[Before] Connecting to mongo\n'.gray);
-      return mongoose.connect('mongodb://testmock.com/TestingDB');
-    });
+    logger.debug('[Before] Preparing storage');
+    return mockgoose
+      .prepareStorage()
+      .then(() => {
+        logger.debug('[Before] Connecting to mongo');
+        return mongoose.connect('mongodb://testmock.com/TestingDB');
+      });
+  });
+  before(function () {
+    // Local components
+    require('../../../app/models/build.js');
+    BuildsController = require('../../../app/controllers/builds.js');
   });
 
   beforeEach(function () {
@@ -39,7 +42,7 @@ describe('controllers/builds.js', function () {
   });
 
   after(function (done) {
-    logger.debug('[After] Closing mongoose connection'.gray);
+    logger.debug('[After] Closing mongoose connection');
     mongoose.disconnect();
     done();
   });
