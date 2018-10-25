eb4b6c84622044cb4675b3c6c30b8e4ea69a08b9
Jussi Vatjus-Anttila <jussiva@gmail.com>
2018-10-25 14:17:40 +0300

    wip

--- app/cluster.js
+++ app/cluster.js
@@ -3,6 +3,8 @@ const Worker = require('./worker');
 const Master = require('./master');
 
 if (cluster.isMaster) {
+  process.once('SIGINT', Master.handleSIGINT);
+  process.once('exit', Master.logMasterDeath);
   Master.onExit = code => process.exit(code);
   Master.initialize();
 } else {
