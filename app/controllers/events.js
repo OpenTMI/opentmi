791e8b4de21d700e0bed5f0e94c5abfdd50240c3
Jussi Vatjus-Anttila <jussiva@gmail.com>
2018-12-10 09:01:00 +0200

    more robust resource handler for events and more regression tests

--- app/controllers/events.js
+++ app/controllers/events.js
@@ -5,6 +5,7 @@
 // native modules
 
 // 3rd party modules
+const Promise = require('bluebird');
 const _ = require('lodash');
 
 // own modules
@@ -54,7 +55,12 @@ class EventsController extends DefaultController {
       .select('cre.date msgid priority.level')
       .cursor()
       .on('data', utilization.push.bind(utilization))
-      .on('error', (err) => { logger.error(err); })
+      .on('error', (error) => {
+        logger.error(`Statistics failure: ${error}`);
+        res
+          .status(500)
+          .json({message: `${error}`, error: error});
+      })
       .on('end', () => res.json(utilization.summary));
   }
   utilization(req, res) {
@@ -70,7 +76,12 @@ class EventsController extends DefaultController {
       .select('cre.date msgid priority.level')
       .cursor()
       .on('data', utilization.push.bind(utilization))
-      .on('error', err => logger.error(err))
+      .on('error', (error) => {
+        logger.error(`Utilization failure: ${error}`);
+        res
+          .status(500)
+          .json({message: `${error}`, error: error});
+      })
       .on('end', () => {
         utilization.calculate()
           .then(res.json.bind(res))
@@ -86,7 +97,7 @@ class EventsController extends DefaultController {
     Promise.fromCallback(cb => this.Model.leanQuery(query, cb))
       .then((events) => { res.json(events); })
       .catch((error) => {
-        logger.warn(error);
+        logger.error(`resourceEvents failure: ${error}`);
         res
           .status(500)
           .json({message: error.message, error: error});
