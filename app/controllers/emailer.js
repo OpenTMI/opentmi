bc77bd78d2fd763aaca0574fabcfa8af883a0e55
Jussi Vatjus-Anttila <jussiva@gmail.com>
2019-01-13 14:56:47 +0200

    eslint cleaning

--- app/controllers/emailer.js
+++ app/controllers/emailer.js
@@ -7,50 +7,51 @@ const logger = require('../tools/logger');
 
 let emailer = {send: () => Promise.reject('emailer is not configured')};
 
+
 class Emailer {
-    constructor(config) {
-        this._smtpTransport = _.get(config, 'enabled') ?
-            this._initialize(config) : Emailer.getDummyTransport();
-        emailer = this;
-    }
-    static getDummyTransport() {
-        return {
-            verify: () => Promise.resolve(),
-            sendMail: () => Promise.reject('smtp is not configured to server')
+  constructor(config) {
+    this._smtpTransport = _.get(config, 'enabled') ?
+      Emailer._initialize(config) : Emailer.getDummyTransport();
+    emailer = this;
+  }
+  static getDummyTransport() {
+    return {
+      verify: () => Promise.resolve(),
+      sendMail: () => Promise.reject('smtp is not configured to server')
+    };
+  }
+  static _initialize(config) {
+    const mailerConfig = _.defaults(config, {
+      host: 'smtp.gmail.com',
+      port: 25,
+      secure: false,
+      connectionTimeout: 5000,
+      tls: {
+        rejectUnauthorized: false
+      }
+    });
+    return nodemailer.createTransport(mailerConfig);
+  }
+  verify() {
+    return this._smtpTransport.verify()
+      .then((value) => {
+        if (value === undefined) {
+          return;
         }
-    }
-    _initialize(config) {
-        const mailerConfig = _.defaults(config, {
-            host: 'smtp.gmail.com',
-            port: 25,
-            secure: false,
-            connectionTimeout: 5000,
-            tls: {
-                rejectUnauthorized: false
-            }
-        });
-        return nodemailer.createTransport(mailerConfig);
-    }
-    verify() {
-        return this._smtpTransport.verify()
-            .then((value) => {
-               if (value === undefined) {
-                   return;
-               }
-               logger.debug('smtp verified - ok');
-            });
-    }
-    static send(data) {
-        return emailer.send(data);
-    }
-    send({to, from = 'admin@opentmi.com', subject, text}) {
-        const mailOptions = {to, from, subject, text};
-        console.log(mailOptions);
-        return this._smtpTransport.sendMail(mailOptions)
-            .then(() => {
-                logger.info(`Sent email to "${to}" with subject: "${subject}"`);
-            });
-    }
+        logger.debug('smtp verified - ok');
+      });
+  }
+  static send(data) {
+    return emailer.send(data);
+  }
+  send({to, from = 'admin@opentmi.com', subject, text}) {
+    const mailOptions = {to, from, subject, text};
+    logger.silly(mailOptions);
+    return this._smtpTransport.sendMail(mailOptions)
+      .then(() => {
+        logger.info(`Sent email to "${to}" with subject: "${subject}"`);
+      });
+  }
 }
 
 module.exports = Emailer;
