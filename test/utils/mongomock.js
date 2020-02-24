c20907ddd5e5dcd2b8cd2497b9be761418ed5929
Jussi Vatjus-Anttila <jussiva@gmail.com>
2020-02-24 18:01:21 +0200

    manage reset properly

--- test/utils/mongomock.js
+++ test/utils/mongomock.js
@@ -5,23 +5,22 @@ const { MongoMemoryServer } = require('mongodb-memory-server');
 mongoose.Promise = Promise;
 
 let mongoServer;
-const opts = {  }; // remove this option if you use mongoose 5 and above
+const opts = { useNewUrlParser: true }; // remove this option if you use mongoose 5 and above
 
 
-module.exports = {
-  setup: async function setup() {
-    mongoServer = new MongoMemoryServer();
-    const mongoUri = await mongoServer.getUri();
-    await mongoose.connect(mongoUri, opts);
-  },
-  reset: async function () {
-    await mongoServer.stop();
-    await mongoServer.start();
-    const mongoUri = await mongoServer.getUri();
-    await mongoose.connect(mongoUri, opts);
-  },
-  teardown: async function teardown() {
-    await mongoose.disconnect();
-    await mongoServer.stop();
-  }
-};
+
+async function setup() {
+  mongoServer = new MongoMemoryServer();
+  const mongoUri = await mongoServer.getUri();
+  await mongoose.connect(mongoUri, opts);
+}
+async function reset() {
+  await teardown();
+  await setup();
+}
+async function teardown() {
+  await mongoose.disconnect();
+  await mongoServer.stop();
+}
+
+module.exports = {setup, reset, teardown};
