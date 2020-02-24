457c8b03ae4dcff3c9f055ca4b19bfbfa53e4391
Jussi Vatjus-Anttila <jussiva@gmail.com>
2020-02-24 18:11:54 +0200

    Update node requirements (#334)
    * update obsoleted new Buffer and silent test logger
    * drop support for node@8 and require node@12
    * replace mockgoose with mongodb-memory-server

--- test/utils/mongomock.js
+++ test/utils/mongomock.js
@@ -1,29 +1,27 @@
 const Promise = require('bluebird');
 const mongoose = require('mongoose');
-const {Mockgoose} = require('mockgoose');
+const {MongoMemoryServer} = require('mongodb-memory-server');
 
 mongoose.Promise = Promise;
-const mockgoose = new Mockgoose(mongoose);
+
+let mongoServer;
+const opts = {useNewUrlParser: true}; // remove this option if you use mongoose 5 and above
 
 
-module.exports = {
-  setup: function setup() {
-    const options = {useNewUrlParser: true};
-    return mockgoose.prepareStorage()
-      .then(() => mongoose.connect('mongodb://test', options));
-  },
-  reset: function () {
-    return mockgoose.helper.reset();
-  },
-  teardown: function teardown() {
-    return mockgoose.helper.reset()
-      .then(() => mongoose.disconnect())
-      .then(() => {
-        const retval = new Promise((resolve) => {
-          mockgoose.mongodHelper.mongoBin.childProcess.once('exit', resolve);
-        }).timeout(2000, 'cannot kill mongoBin process');
-        mockgoose.mongodHelper.mongoBin.childProcess.kill('SIGTERM');
-        return retval;
-      });
-  }
-};
+async function setup() {
+  mongoServer = new MongoMemoryServer();
+  const mongoUri = await mongoServer.getUri();
+  await mongoose.connect(mongoUri, opts);
+}
+
+async function teardown() {
+  await mongoose.disconnect();
+  await mongoServer.stop();
+}
+
+async function reset() {
+  await teardown();
+  await setup();
+}
+
+module.exports = {setup, reset, teardown};
